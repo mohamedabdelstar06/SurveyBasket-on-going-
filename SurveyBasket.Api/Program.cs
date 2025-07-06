923533c4cacaeb103bf42cd3cd5c7d495f2d584d
MohamedAbdelstar270 <mabdelstar270@gmail.com>
2025-07-06 14:22:09 +0300

    Add project files.

---
+++ SurveyBasket.Api/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi(); // Maps /openapi/v1.json and /docs (optional UI endpoint)
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
